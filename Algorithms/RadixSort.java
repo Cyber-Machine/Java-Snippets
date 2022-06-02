533c1b1e70b0dd67011cb3a3a3936435c82278bd
Maaz Karim <MaazKarim02@gmail.com>
2022-06-03 02:32:46 +0530

    First Commit

---
+++ Algorithms/RadixSort.java
@@ -0,0 +1,73 @@
+package Algorithms;
+/**
+ * RadixSort
+ * RadixSort or Counting sort is a faster sorting algoritm that sorts all positive integers in O(k) time , k is the length of digits.
+ */
+public interface RadixSort {
+    public static long[] radixSort(long[] f){ return radixSort(f, f.length); }
+	public static long[] radixSort(long[] f, int n)
+	{
+		long[] to = new long[n];
+		{
+			int[] b = new int[65537];
+			for(int i = 0;i < n;i++)b[1+(int)(f[i]&0xffff)]++;
+			for(int i = 1;i <= 65536;i++)b[i]+=b[i-1];
+			for(int i = 0;i < n;i++)to[b[(int)(f[i]&0xffff)]++] = f[i];
+			long[] d = f; f = to;to = d;
+		}
+		{
+			int[] b = new int[65537];
+			for(int i = 0;i < n;i++)b[1+(int)(f[i]>>>16&0xffff)]++;
+			for(int i = 1;i <= 65536;i++)b[i]+=b[i-1];
+			for(int i = 0;i < n;i++)to[b[(int)(f[i]>>>16&0xffff)]++] = f[i];
+			long[] d = f; f = to;to = d;
+		}
+		{
+			int[] b = new int[65537];
+			for(int i = 0;i < n;i++)b[1+(int)(f[i]>>>32&0xffff)]++;
+			for(int i = 1;i <= 65536;i++)b[i]+=b[i-1];
+			for(int i = 0;i < n;i++)to[b[(int)(f[i]>>>32&0xffff)]++] = f[i];
+			long[] d = f; f = to;to = d;
+		}
+		{
+			int[] b = new int[65537];
+			for(int i = 0;i < n;i++)b[1+(int)(f[i]>>>48&0xffff)]++;
+			for(int i = 1;i <= 65536;i++)b[i]+=b[i-1];
+			for(int i = 0;i < n;i++)to[b[(int)(f[i]>>>48&0xffff)]++] = f[i];
+			long[] d = f; f = to; to = d;
+		}
+		return f;
+	}
+
+	
+
+	public static int[] radixSort2(int[] a)
+	{
+		int n = a.length;
+		int[] c0 = new int[0x101];
+		int[] c1 = new int[0x101];
+		int[] c2 = new int[0x101];
+		int[] c3 = new int[0x101];
+		for(int v : a) {
+			c0[(v&0xff)+1]++;
+			c1[(v>>>8&0xff)+1]++;
+			c2[(v>>>16&0xff)+1]++;
+			c3[(v>>>24^0x80)+1]++;
+		}
+		for(int i = 0;i < 0xff;i++) {
+			c0[i+1] += c0[i];
+			c1[i+1] += c1[i];
+			c2[i+1] += c2[i];
+			c3[i+1] += c3[i];
+		}
+		int[] t = new int[n];
+		for(int v : a)t[c0[v&0xff]++] = v;
+		for(int v : t)a[c1[v>>>8&0xff]++] = v;
+		for(int v : a)t[c2[v>>>16&0xff]++] = v;
+		for(int v : t)a[c3[v>>>24^0x80]++] = v;
+		return a;
+	}
+
+
+    
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+## Snippets for Java Problem Solving
+
+This repo contains some code snippets which I use while practising problem in Java.
+
+This Repo contains various Data Structures and Algorithms written in Java (More Upcoming soon!).
+
+-------
+### Credits
+#### Websites 
+- [cp-algorithms](https://cp-algorithms.com/)
+- [GeeksForGeeks](https://www.geeksforgeeks.org/)
+
+---------
+### Contribution
+- Feel free to suggest and improve the code base by opening an issue.
