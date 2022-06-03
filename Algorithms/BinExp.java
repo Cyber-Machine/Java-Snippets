c45d6dce9692900c5e6cb2ac8d05b68405a6981e
Maaz Karim <MaazKarim02@gmail.com>
2022-06-03 21:04:47 +0530

    Added BinExp

---
+++ Algorithms/BinExp.java
@@ -0,0 +1,20 @@
+package Algorithms;
+
+/**
+ * Binary Exponention calculates base^exp in log(n) time
+ * https://cp-algorithms.com/algebra/binary-exp.html
+ */
+public interface BinExp {
+    public static long FastExp(long base, long exp) {
+        long ans=1;
+        long mod=998244353;
+        while (exp>0) {
+            if (exp%2==1) ans*=base;
+            exp/=2;
+            base*=base;
+            base%=mod;
+            ans%=mod;
+        }
+        return ans;
+    }
+}
