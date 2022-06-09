690b25bc5ea188d9fc7bd77f1ca2f5ebd802fb84
Maaz Karim <MaazKarim02@gmail.com>
2022-06-09 17:21:16 +0530

    Updated Indendation

--- Algorithms/GCD.java
+++ Algorithms/GCD.java
@@ -16,58 +16,58 @@ public interface GCD {
 		return a;
 	}
 
-			// H.C.F. by Binary Method(Eucladian Algorithm)
-			private static int gcdBinary(int x, int y)
-			{
-			  int shift;
-		  
-			  /* GCD(0, y) == y; GCD(x, 0) == x, GCD(0, 0) == 0 */
-			  if (x == 0)
-				return y;
-			  if (y == 0)
-				return x;
-		  
-			  int gcdX = Math.abs(x);
-			  int gcdY = Math.abs(y);
-		  
-			  if (gcdX == 1 || gcdY == 1)
-				return 1;
-		  
-			  /* Let shift := lg K, where K is the greatest power of 2 dividing both x and y. */
-			  for (shift = 0; ((gcdX | gcdY) & 1) == 0; ++shift)
-			  {
-				gcdX >>= 1;
-				gcdY >>= 1;
-			  }
-		  
-			  while ((gcdX & 1) == 0)
-				gcdX >>= 1;
-		  
-			  /* From here on, gcdX is always odd. */
-			  do
-			  {
-				/* Remove all factors of 2 in gcdY -- they are not common */
-				/* Note: gcdY is not zero, so while will terminate */
-				while ((gcdY & 1) == 0)
-				  /* Loop X */
-				  gcdY >>= 1;
-		  
-				/*
-				 * Now gcdX and gcdY are both odd. Swap if necessary so gcdX <= gcdY,
-				 * then set gcdY = gcdY - gcdX (which is even). For bignums, the
-				 * swapping is just pointer movement, and the subtraction
-				 * can be done in-place.
-				 */
-				if (gcdX > gcdY)
-				{
-				  final int t = gcdY;
-				  gcdY = gcdX;
-				  gcdX = t;
-				}  // Swap gcdX and gcdY.
-				gcdY = gcdY - gcdX;                       // Here gcdY >= gcdX.
-			  }while (gcdY != 0);
-		  
-			  /* Restore common factors of 2 */
-			  return gcdX << shift;
-			}
+	// H.C.F. by Binary Method(Eucladian Algorithm)
+	private static int gcdBinary(int x, int y)
+	{
+	  int shift;
+	
+	  /* GCD(0, y) == y; GCD(x, 0) == x, GCD(0, 0) == 0 */
+	  if (x == 0)
+		return y;
+	  if (y == 0)
+		return x;
+	
+	  int gcdX = Math.abs(x);
+	  int gcdY = Math.abs(y);
+	
+	  if (gcdX == 1 || gcdY == 1)
+		return 1;
+	
+	  /* Let shift := lg K, where K is the greatest power of 2 dividing both x and y. */
+	  for (shift = 0; ((gcdX | gcdY) & 1) == 0; ++shift)
+	  {
+		gcdX >>= 1;
+		gcdY >>= 1;
+	  }
+	
+	  while ((gcdX & 1) == 0)
+		gcdX >>= 1;
+	
+	  /* From here on, gcdX is always odd. */
+	  do
+	  {
+		/* Remove all factors of 2 in gcdY -- they are not common */
+		/* Note: gcdY is not zero, so while will terminate */
+		while ((gcdY & 1) == 0)
+		  /* Loop X */
+		  gcdY >>= 1;
+	
+		/*
+		 * Now gcdX and gcdY are both odd. Swap if necessary so gcdX <= gcdY,
+		 * then set gcdY = gcdY - gcdX (which is even). For bignums, the
+		 * swapping is just pointer movement, and the subtraction
+		 * can be done in-place.
+		 */
+		if (gcdX > gcdY)
+		{
+		  final int t = gcdY;
+		  gcdY = gcdX;
+		  gcdX = t;
+		}  // Swap gcdX and gcdY.
+		gcdY = gcdY - gcdX;                       // Here gcdY >= gcdX.
+	  }while (gcdY != 0);
+	
+	  /* Restore common factors of 2 */
+	  return gcdX << shift;
+	}
 }
